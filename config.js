0594eb3b4cd2502e1493020ad9d8e65faa83ae40
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:20:46 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FLORENCE + THE MACHINE';
+export const nameSongs = [
+	"HUNGER", /*1*/
+	"SHAKE IT OUT", /*2*/
+	"DOG DAYS ARE OVER", /*3*/
+	"SPECTRUM", /*4*/
+	"COSMIC LOVE", /*5*/
+	"FREE", /*6*/
+	"YOU'VE GOT THE LOVE", /*7*/
+	"GIRL WITH ONE EYE", /*8*/
+	"DREAM GIRL EVIL", /*9*/
+	"KING", /*10*/
+	"RABBIT HEART", /*11*/
+	"BIRD SONG", /*12*/
+	"NEVER LET ME GO", /*13*/
+	"PURE FEELING", /*14*/
+	"NO LIGHT, NO LIGHT", /*15*/
+	"WHICH WITCH", /*16*/
+	"SEVEN DEVILS", /*17*/
+	"WHAT THE WATER GAVE ME", /*18*/
+	"SHIP TO WRECK", /*19*/
+	"BREATH OF LIFE", /*20*/
+]
